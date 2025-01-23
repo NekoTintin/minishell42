7e6f5015be621990fccd996d8aeb7921f052f77c
NekoTintin <tintin361official@gmail.com>
2025-01-23 13:30:03 +0100

    limits

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,15 +6,16 @@
 /*   By: qupollet <qupollet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 14:47:36 by qupollet          #+#    #+#             */
-/*   Updated: 2025/01/21 21:39:18 by qupollet         ###   ########.fr       */
+/*   Updated: 2025/01/23 13:29:22 by qupollet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+#include <linux/limits.h>
 
 char	*ft_pwd(void)
 {
-	char		buffer[4096];
+	char		buffer[PATH_MAX];
 	char		*pwd;
 
 	if (getcwd(buffer, sizeof(buffer)) == NULL)
