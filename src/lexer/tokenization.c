8e393a207ab3dce5311a346bb0b04dc6cea758db
Ubuntu <benoit.challat@gmail.com>
2025-01-24 05:42:07 +0100

    tokenization.c after write fonc le_conditional_assing

--- src/lexer/tokenization.c
+++ src/lexer/tokenization.c
@@ -6,7 +6,7 @@
 /*   By: unbuntu <unbuntu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 20:25:00 by unbuntu           #+#    #+#             */
-/*   Updated: 2025/01/24 05:25:18 by unbuntu          ###   ########.fr       */
+/*   Updated: 2025/01/24 05:37:59 by unbuntu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,117 @@
 static char    *ft_strndup(const char *s1, size_t n);
 static t_tokenization     *le_conditional_assing(t_tokenization *curr, char *str, t_token_type type, int length);
 
+int         le_assing_word(t_lexer *lexer, char *string, int index)
+{
+    int     end_index;
+    int     length;
+    t_tokenization  *curr;
+
+    end_index = le_find_word(string, index);
+    length = end_index - index;
+    lexer = ft_add_token(lexer);
+    curr = le_last_node(lexer);
+    curr = le_conditional_assing(curr, string[index], WORD, length);
+    return(end_index + 1);
+}
+
+int         le_assing_quotes(t_lexer *lexer, char *string, int index)
+{
+    int     end_index;
+    int     length;
+    t_tokenization  *curr;
+
+    end_index = le_find_word(string, index);
+    length = end_index - index;
+    lexer = ft_add_token(lexer);
+    curr = le_last_node(lexer);
+    curr = le_conditional_assing(curr, string[index], QUOTES, length);
+    return(end_index + 1);
+}
+
+int         le_assing_synbols(t_lexer *lexer, char *string, int index)
+{
+    int             end_index;
+    t_tokenization  *curr;
+
+    end_index = le_identify_synbols(string, index);
+    lexer = ft_add_token(lexer);
+    curr = le_last_node(lexer);
+    if (end_index - index == 2)
+    {
+        if (string[index] == '<')
+            curr = le_conditional_assing(curr, string[index], REDIRECT_IN, end_index - index);
+        if (string[index] == '>')
+            curr = le_conditional_assing(curr, string[index], REDIRECT_OUT, end_index - index);
+    }
+    else if (string[index] == '<')
+            curr = le_conditional_assing(curr, string[index], HEREDOC, end_index - index);
+    else if (string[index] == '>')
+            curr = le_conditional_assing(curr, string[index], APPEND, end_index - index);
+    else if (string[index] == '|')
+            curr = le_conditional_assing(curr, string[index], PIPE, end_index - index);
+    return (end_index);
+}
+
+static t_tokenization     *le_conditional_assing(t_tokenization *curr, char *str, t_token_type type, int length)
+{
+        curr->value = ft_strdnup(str, length);
+        if (curr->value != NULL)
+            return(NULL);
+        curr->type = type;
+        return (curr);
+}
+
+static char    *ft_strndup(const char *s1, size_t n)
+{
+    char    *dup;
+    size_t  i;
+
+    i = 0;
+    while (s1[i] && i < n)
+        i++;
+    dup = (char *)malloc(sizeof(char) * (i + 1)); // +1 pour le '\0'
+    if (!dup)
+        return (NULL);
+    i = 0;
+    while (s1[i] && i < n)
+    {
+        dup[i] = s1[i];
+        i++;
+    }
+    dup[i] = '\0';
+    return (dup);
+}
+
+/*
+int         le_assing_synbols(t_lexer *lexer, char *string, int index)
+{
+    int             end_index;
+    char            *synbol;
+    t_tokenization  *curr;
+
+    end_index = le_identify_synbols(string, index);
+    synbol = (char *)malloc(sizeof(char) * (end_index - index) + 1);
+    lexer = ft_add_token(lexer);
+    curr = le_last_node(lexer);
+    if (end_index - index == 2)
+    {
+        if (string[index] == '<')
+            curr = le_conditional_assing(curr, string[index], REDIRECT_IN, end_index - index);
+        if (string[index] == '>')
+            curr = le_conditional_assing(curr, string[index], REDIRECT_OUT, end_index - index);
+    }
+    else if (string[index] == '<')
+            curr = le_conditional_assing(curr, string[index], HEREDOC, end_index - index);
+    else if (string[index] == '>')
+            curr = le_conditional_assing(curr, string[index], APPEND, end_index - index);
+    else if (string[index] == '|')
+            curr = le_conditional_assing(curr, string[index], PIPE, end_index - index);
+    return (end_index);
+}
+*/
+
+/*
 int         le_assing_word(t_lexer *lexer, char *string, int index)
 {
     int     end_index;
@@ -42,7 +153,9 @@ int         le_assing_word(t_lexer *lexer, char *string, int index)
     free(word);
     return(end_index + 1);
 }
+*/
 
+/*
 int         le_assing_quotes(t_lexer *lexer, char *string, int index)
 {
         int     end_index;
@@ -71,57 +184,4 @@ int         le_assing_quotes(t_lexer *lexer, char *string, int index)
     return(end_index + 1);
 
 }
-
-int         le_assing_synbols(t_lexer *lexer, char *string, int index)
-{
-    int             end_index;
-    char            *synbol;
-    t_tokenization  *curr;
-
-    end_index = le_identify_synbols(string, index);
-    synbol = (char *)malloc(sizeof(char) * (end_index - index) + 1);
-    lexer = ft_add_token(lexer);
-    curr = le_last_node(lexer);
-    if (end_index - index == 2)
-    {
-        if (string[index] == '<')
-            curr = le_conditional_assing(curr, string[index], REDIRECT_IN, end_index - index);
-        if (string[index] == '>')
-            curr = le_conditional_assing(curr, string[index], REDIRECT_OUT, end_index - index);
-    }
-    else if (string[index] == '<')
-            curr = le_conditional_assing(curr, string[index], HEREDOC, end_index - index);
-    else if (string[index] == '>')
-            curr = le_conditional_assing(curr, string[index], APPEND, end_index - index);
-    else if (string[index] == '|')
-            curr = le_conditional_assing(curr, string[index], PIPE, end_index - index);
-    return (end_index);
-}
-
-static t_tokenization     *le_conditional_assing(t_tokenization *curr, char *str, t_token_type type, int length)
-{
-        curr->value = ft_strdnup(str, length);
-        curr->type = type;
-        return (curr);
-}
-
-static char    *ft_strndup(const char *s1, size_t n)
-{
-    char    *dup;
-    size_t  i;
-
-    i = 0;
-    while (s1[i] && i < n)
-        i++;
-    dup = (char *)malloc(sizeof(char) * (i + 1)); // +1 pour le '\0'
-    if (!dup)
-        return (NULL);
-    i = 0;
-    while (s1[i] && i < n)
-    {
-        dup[i] = s1[i];
-        i++;
-    }
-    dup[i] = '\0';
-    return (dup);
-}
+*/
