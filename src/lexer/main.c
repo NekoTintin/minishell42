1a73fbd2b82797c3fa80243ea9cfdf21fdd2f8f5
Ubuntu <benoit.challat@gmail.com>
2025-01-27 12:43:48 +0100

    update testeur

--- src/lexer/main.c
+++ src/lexer/main.c
@@ -6,13 +6,14 @@
 /*   By: unbuntu <unbuntu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:16:32 by unbuntu           #+#    #+#             */
-/*   Updated: 2025/01/25 16:43:12 by unbuntu          ###   ########.fr       */
+/*   Updated: 2025/01/27 12:42:58 by unbuntu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
 static void     print_chain(t_lexer *lexer);
+static char     print_enum(int nb);
 
 int     main(int argc, char **argv)
 {
@@ -26,12 +27,41 @@ int     main(int argc, char **argv)
 
 static void    print_chain(t_lexer *lexer)
 {
+    int     index = 0;
     t_token *node = lexer->header;
 
     while (node != NULL)
     {
+        printf("======node:%d======\n", index);
         printf("-> valeur node: %s \n", node->value);
-        printf("-> type   node: %d \n", node->type);
+        printf("-> type   node:");
+        print_enum(node->type);
         node = node->next;
     }
+}
+
+static char     print_enum(int nb)
+{
+    if (nb == 0)
+        printf("WORD\n");
+    if (nb == 1)
+        printf("VAR_ENV\n");
+    if (nb == 2)
+        printf("S_QUOTES\n");
+    if (nb == 3)
+       printf("D_QUOTES\n");
+    if (nb == 4)
+        printf("PIPE\n");
+    if (nb == 5)
+        printf("REDIRECT_IN\n");
+    if (nb == 6)
+        printf("REDIRECT_OUT\n");
+    if (nb == 7)
+        printf("APPEND\n");
+    if (nb == 8)
+        printf("HEREDOC\n");
+    if (nb == 9)
+        printf("WHITESPACE\n");
+    if (nb == 10)
+       printf("UNKNOWN\n");
 }
