e28546af05a0a4a23b5e9211af7c4c4d84e91be1
NekoTintin <tintin361official@gmail.com>
2025-01-29 21:04:48 +0100

    yep

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,9 +6,36 @@
 /*   By: qupollet <qupollet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 00:45:21 by qupollet          #+#    #+#             */
-/*   Updated: 2025/01/24 00:45:36 by qupollet         ###   ########.fr       */
+/*   Updated: 2025/01/29 20:52:42 by qupollet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+#include "lexer.h"
+#include "parsing.h"
 
+int	ft_create_commands(t_lexer *lexer, t_parser *parser)
+{
+	int		iterator;
+	int		nb_commands;
+
+	iterator = 0;
+	nb_commands = ft_command_counter(lexer);
+	if (nb_commands < 0)
+		return (-1);
+	while (iterator < nb_commands)
+		if (ft_init_command(parser) == -1)
+			return (-1);
+	return (0);
+}
+
+t_parser	*parsing(t_lexer *lexer)
+{
+	t_parser	*parser;
+
+	parser = ft_init_parser();
+	if (!parser)
+		return (NULL);
+	(void)lexer;
+	return (parser);
+}
---
+++ src/parsing/t_command_utils.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_command_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qupollet <qupollet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 19:38:11 by qupollet          #+#    #+#             */
+/*   Updated: 2025/01/29 20:04:29 by qupollet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+#include "parsing.h"
+#include "../../includes/minishell.h"
+
+t_parser	*ft_init_parser(void)
+{
+	t_parser	*parser;
+
+	parser = ft_calloc(1, sizeof(t_parser));
+	if (!parser)
+		return (NULL);
+	parser->size = 0;
+	parser->top = NULL;
+	return (parser);
+}
+
+t_cmd	*ft_parser_last(t_parser *parser)
+{
+	t_cmd	*cur;
+
+	cur = parser->top;
+	while (cur && cur->next)
+		cur = cur->next;
+	return (cur);
+}
+
+int	ft_init_command(t_parser *parser)
+{
+	t_cmd	*command;
+	t_cmd	*last;
+
+	if (!parser)
+		return (-1);
+	command = ft_calloc(1, sizeof(t_cmd));
+	if (!command)
+		return (-1);
+	command->append = -1;
+	command->args = NULL;
+	command->infile = NULL;
+	command->outfile = NULL;
+	command->next = NULL;
+	if (!parser->top)
+		parser->top = command;
+	else
+	{
+		last = ft_parser_last(parser);
+		last->next = command;
+	}
+	parser->size++;
+	return (0);
+}
+
+void	ft_free_parser(t_parser *parser)
+{
+	t_cmd		*cur;
+	t_cmd		*next;
+	int			i;
+
+	if (!parser)
+		return ;
+	cur = parser->top;
+	while (cur)
+	{
+		next = cur->next;
+		free(cur->next);
+		if (cur->infile)
+			free(cur->infile);
+		if (cur->outfile)
+			free(cur->outfile);
+		if (cur->args)
+		{
+			i = -1;
+			while (cur->args[++i])
+				free(cur->args[i]);
+		}
+		free(cur);
+		cur = next;
+	}
+	free(parser);
+}
