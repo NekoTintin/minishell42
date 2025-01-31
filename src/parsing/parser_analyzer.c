0795693f4c2357ee53e98a9b891f5cefd1c24681
NekoTintin <tintin361official@gmail.com>
2025-01-31 18:38:35 +0100

    again

--- src/parsing/parser_analyzer.c
+++ src/parsing/parser_analyzer.c
@@ -6,7 +6,7 @@
 /*   By: qupollet <qupollet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 17:16:05 by qupollet          #+#    #+#             */
-/*   Updated: 2025/01/31 17:22:32 by qupollet         ###   ########.fr       */
+/*   Updated: 2025/01/31 18:10:51 by qupollet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,40 @@
 #include "lexer.h"
 #include "parsing.h"
 
+int	ft_add_arg(t_cmd *cmd, char *arg)
+{
+	
+}
+
 int	ft_is_symbol(t_token *token, t_cmd *cmd)
 {
+	if (token->type == REDIRECT_IN)
+		return (1);
+	else if (token->type == REDIRECT_OUT)
+		return (2);
+	else if (token->type == APPEND)
+		return (3);
+	else if (token->type == HEREDOC)
+		return (4);
+	return (-1);
+}
+
+int	ft_token_analyzer(t_token *token, t_cmd *cmd)
+{
+	static int	redirect_mode = 0;
+
 	if (!cmd || !token)
 		return (-1);
 	if (token->type == REDIRECT_IN)
 		cmd->infile = token->next->value;
 	else if (token->type == REDIRECT_OUT)
 		cmd->outfile = token->next->value;
+	else if (token->type == APPEND)
+	{
+		cmd->append = 1;
+	}
 	else
 		return (0);
 	return (1);
+	return (0);
 }
