be064a3421c8f4754360548409e091812c0361fc
Ubuntu <benoit.challat@gmail.com>
2025-02-14 18:40:19 +0100

    add fonc lx_clean_lexer()

---
+++ src/lexer/lx_clean_lexer.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lx_clean_lexer.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unbuntu <unbuntu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 19:16:44 by benoitchall       #+#    #+#             */
+/*   Updated: 2025/02/14 18:27:31 by unbuntu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+
+t_lexer	*lx_clean_lexer(t_lexer *lexer)
+{
+	t_token	*curr;
+	t_token	*free_node;
+
+	curr = lexer->header;
+	free_node = NULL;
+	while (curr->next != NULL)
+	{
+		if (curr->next->type == WHITESPACE)
+		{
+			free_node = curr->next;
+			curr->next = curr->next->next;
+			free(free_node->value);
+			free(free_node);
+			lexer->size -= 1;
+		}
+		else
+			curr = curr->next;
+	}
+	return (lexer);
+}
