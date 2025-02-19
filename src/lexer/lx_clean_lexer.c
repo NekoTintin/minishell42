b5fee4bf3c8d2b824f75b7c8e8887d63a1a6d42e
Ubuntu <benoit.challat@gmail.com>
2025-02-19 18:20:02 +0100

    corection lexer

--- src/lexer/lx_clean_lexer.c
+++ src/lexer/lx_clean_lexer.c
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   lx_clean_lexer.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: unbuntu <unbuntu@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bchallat <bchallat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 19:16:44 by benoitchall       #+#    #+#             */
-/*   Updated: 2025/02/14 18:27:31 by unbuntu          ###   ########.fr       */
+/*   Updated: 2025/02/19 18:18:50 by bchallat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lexer.h"
+#include "../../includes/lexer.h"
 
 t_lexer	*lx_clean_lexer(t_lexer *lexer)
 {
 	t_token	*curr;
 	t_token	*free_node;
 
+	if (lexer == NULL || lexer->header == NULL)
+		return (NULL);
 	curr = lexer->header;
 	free_node = NULL;
 	while (curr->next != NULL)
