a3e0aefe3612dfc3f2708d93951ea1de712df0b7
Benoit Challat <benoitchallat@MacBook-Pro-de-Benoit.local>
2025-03-03 17:15:30 +0100

    write fonction for free struct

--- src/parsing/free_element_struct.c
+++ src/parsing/free_element_struct.c
@@ -15,3 +15,62 @@
 void	free_all_parser(t_parser *parser);
 void	free_all_command(t_cmd *cmd);
 void	free_all_redirect(t_redirect *redirect);
+static void	free_array(char **array);
+
+void	free_all_parser(t_parser *parser)
+{
+	if (parser != NULL)
+	{
+		free_all_command(parser->top);
+		free(parser);
+	}
+}
+
+void	free_all_command(t_cmd *cmd)
+{
+	t_cmd	*curr_f;
+
+	if (cmd != NULL)
+	{
+		while (cmd != NULL)
+		{
+			curr_f = cmd;
+			cmd = cmd->next;
+			free_array(curr_f->argument);
+			free_all_redirect(curr_f->redirect);
+			free(curr_f);
+		}
+	}
+}
+
+void	free_all_redirect(t_redirect *redirect)
+{
+	t_redirect	*curr_f;
+
+	if (redirect != NULL)
+	{
+		while (redirect != NULL)
+		{
+			curr_f = redirect;
+			redirect = redirect->next;
+			free_array(curr_f->file);
+			free(curr_f);
+		}
+	}
+}
+
+static void	free_array(char **array)
+{
+	int	index;
+
+	if (array != NULL)
+	{
+		index = 0;
+		while (array[index] != NULL)
+		{
+			free(array[index]);
+			index++;
+		}
+		free(array);
+	}
+}
